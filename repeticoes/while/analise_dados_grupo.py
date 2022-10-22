48d428c5adc75d38be0e4aec2cb04624711562a4
Victor "Noobzin <vitorusko@gmail.com>
2022-10-22 00:30:26 -0300

    update exercicios

--- repeticoes/while/analise_dados_grupo.py
+++ repeticoes/while/analise_dados_grupo.py
@@ -8,4 +8,34 @@ B) quantos homens foram cadastrados.
 C) quantas mulheres tem menos de 20 anos.
 '''
 
+import os 
+os.system('cls')
 
+
+homens = mulheres = pessoas_18 = 0
+print('''-------------------------
+CADASTRE UMA PESSOA
+-------------------------''')
+
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    continuar = ' '
+    while sexo not in 'MF':
+        sexo = input('Sexo:  [M/F] ').strip().upper()[0]
+        if idade >= 18:
+            pessoas_18+=1
+            
+        if sexo == 'M':
+            homens+=1
+
+        if idade < 20 and sexo == 'F':
+            mulheres+=1
+
+
+    while continuar not in 'SN':
+        continuar = input('Quer continuar? [S/N] ').upper().strip()[0]
+    if continuar == 'N':
+        break
+    
+print(f'Total de pessoas com mais de 18 anos: {pessoas_18}\nAo todo temos {homens} homens cadastrados\nE temos {mulheres} mulheres com menos de 20 anos')
